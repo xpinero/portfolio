dbe838f7f97200fe03eaa046dad42fbfa1b0c7c5
Xaver Pinero <xaverpinero@yahoo.com>
2022-02-16 15:52:38 -0500

    layout for about me component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Intro from "./components/intro/Intro";
+import About from "./components/about/About";
 
 function App() {
   return (
     <div>
-      <Intro></Intro>
+      <Intro />
+      <About />
     </div>
   );
 }
---
+++ portfolio/src/components/about/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./about.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-left">
+        <div className="about-card"></div>
+        <div className="about-card">
+          
+        </div>
+      </div>
+      <div className="about-right"></div>
+    </div>
+  );
+};
+
+export default About;
